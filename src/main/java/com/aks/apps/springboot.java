e834a69813115f43d34c3c7afc61b5006eb91d89
Shiva DIXIT <skdixit@us.ibm.com>
2020-06-05 21:15:22 -0700

    New changes

---
+++ src/main/java/com/aks/apps/springboot.java
@@ -0,0 +1,14 @@
+package com.aks.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springboot {
+	
+	public void main(String[] args)
+	{
+		SpringApplication.run(springboot.class, args);
+	}
+
+}
